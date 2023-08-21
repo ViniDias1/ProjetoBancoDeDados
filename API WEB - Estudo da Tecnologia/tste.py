a341bfdb8dd8d6ea4672011eea5872e19867badd
ViniDias <90332822+ViniDias1@users.noreply.github.com>
2023-08-21 14:37:46 -0300

    Add files via upload

---
+++ API WEB - Estudo da Tecnologia/tste.py
@@ -0,0 +1,21 @@
+teste = [
+    {
+        "666": {
+            "cpf": 666,
+            "nome": "SIMONE DIAS",
+            "data_nascimento": "05/12/2002"
+        },
+        "222": {
+            "cpf": 222,
+            "nome": "Vin\u00edcius",
+            "data_nascimento": "05/12/2002"
+        },
+        "999": {
+            "cpf": 999,
+            "nome": "teste",
+            "data_nascimento": "05/12/2002"
+        }
+    }
+]
+
+print(teste[0]["222"])

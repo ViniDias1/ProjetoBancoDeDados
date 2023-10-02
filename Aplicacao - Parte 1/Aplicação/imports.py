cc79554e17e868ef3742e18cce2dbcbd92b40e70
ViniDias <90332822+ViniDias1@users.noreply.github.com>
2023-10-02 17:30:05 -0300

    Update imports.py

--- Aplicacao - Parte 1/Aplicação/imports.py
+++ Aplicacao - Parte 1/Aplicação/imports.py
@@ -3,8 +3,6 @@ import json
 import mysql.connector
 from random import randint
 
-
-
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
@@ -14,5 +12,13 @@ conexao = mysql.connector.connect(
     password = "Professor123.",
     database = "mydb"
 )
-
 cargos = ['estagiario','supervisor','gestor','gerente','fiscal']
+
+cargos_funcao = {
+    'estagiario':'alterou quantidade de lotes',
+    'supervisor': 'Autorizou Leilão',
+    'gestor': 'Autorizou Lotes',
+    'gerente': 'Coordenou Leilão',
+    'fiscal': 'Fiscalizou os Lotes'
+}
+

7ed988ec8ec898be3c6829be419c5881b182535e
ViniDias <90332822+ViniDias1@users.noreply.github.com>
2023-10-03 17:53:37 -0300

    Update crud_Usuario.py

--- Aplicacao - Parte 1/Aplicação/crud_Usuario.py
+++ Aplicacao - Parte 1/Aplicação/crud_Usuario.py
@@ -2,6 +2,7 @@ from imports import *
 
 @app.route('/create_Usuario', methods=['POST'])
 def create_Usuario():
+
 	cursor = conexao.cursor()
 	cpf = str(randint(1,1000))
 	numero = randint(1,1000)
@@ -13,13 +14,15 @@ def create_Usuario():
 	conexao.commit()
 	cursor.close()
 	return make_response("Usuario Cadastrado!")
-# SELECT todos os usuarios
+
 @app.route('/selectAll_Usuario', methods=['GET'])
 def selectAll_Usuario():
 
 	cursor = conexao.cursor()
+
 	sql = "SELECT * FROM Usuario"
 	cursor.execute(sql)
+
 	resposta = cursor.fetchall()
 	cursor.close()
 	return make_response(
@@ -28,13 +31,14 @@ def selectAll_Usuario():
 		)
 	)
 
-# SELECT por cpf
 @app.route('/selectUsuario_cpf/<int:cpf>', methods=['GET'])
 def selectUsuario_cpf(cpf):
 
 	cursor = conexao.cursor()
+
 	sql = f"SELECT * FROM Usuario WHERE cpf = {cpf}"
 	cursor.execute(sql)
+
 	resposta = cursor.fetchall()
 	cursor.close()
 	return make_response(
@@ -43,25 +47,26 @@ def selectUsuario_cpf(cpf):
 		)
 	)
 
-#UPDATE 
 @app.route('/update_Senha_Usuario/<int:cpf>', methods=['PUT'])
 def update_Senha_Usuario(cpf):
+
 	cursor = conexao.cursor()
+
 	sql = f"UPDATE Usuario SET senha = 'novaSENHA' WHERE cpf = {cpf}"
 	cursor.execute(sql)
+
 	conexao.commit()
 	cursor.close()
 	return make_response("Senha Alterada!")
 
-
-#DELETE usuario
 @app.route('/delete_Usuario/<int:cpf>', methods=['DELETE'])
 def delete_Usuario(cpf):
+
 	cursor = conexao.cursor()
+
 	sql = f"DELETE FROM Usuario WHERE cpf = {cpf}"
 	cursor.execute(sql)
+
 	conexao.commit()
 	cursor.close()
 	return make_response("Usuario deletado!")
-
-# ---- TABELA USUARIO ---- #

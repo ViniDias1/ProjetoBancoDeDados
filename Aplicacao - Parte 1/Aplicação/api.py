124e195bde1a81d112aecc457284f8da0762f24d
ViniDias <90332822+ViniDias1@users.noreply.github.com>
2023-10-03 17:52:06 -0300

    Update api.py

--- Aplicacao - Parte 1/Aplicação/api.py
+++ Aplicacao - Parte 1/Aplicação/api.py
@@ -2,8 +2,9 @@ import crud_Usuario
 import crud_Usuario_Admin
 import crud_Leilao
 import crud_Gerencia_Leilao
+import transacao
 from imports import *
 
 if __name__ == '__main__':
 		app.run()
-	
+ 
